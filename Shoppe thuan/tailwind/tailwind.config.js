22aba90ba2d26a8ca7bad2420d607c14d3d90e93
ducanhlaminh <89836104+ducanhlaminh@users.noreply.github.com>
2023-01-09 00:56:05 +0700

    commit

---
+++ Shoppe thuan/tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "**/*.html"
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
